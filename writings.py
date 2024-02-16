0e6d1288e635eae9e444b5bc9c5f1a10539906fe
mrzgardini <33785639+mrzgardini@users.noreply.github.com>
2024-02-16 20:02:20 +0100

    Update writings.py

--- writings.py
+++ writings.py
@@ -1,11 +1,22 @@
 import datetime
+import re
 
-o = open("index.htm", "r")
-righe = o.readlines()
-i = open("input.txt", "r")
-righe1 = i.read()
-testo = '\n<div' + ' ' + 'id=\'' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\'>\n' + '<p style=\'font-size:10px\'>\n' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n</p>\n' + '<p>\n' + righe1 + '\n</p>\n</div>\n<br>\n\n'
-righe[8] += testo
-o = open("index.htm", "w")
-o.writelines(righe)
-o.close()
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+a = open("index.htm", "r")
+b = a.readlines()
+c = open("input.txt", "r")
+d = c.read()
+e = re.sub(f"\n", f"<br> ", d)
+f = '\n<div' + ' ' + 'id=\'' + timestamp + '\'>\n' + '<p style=\'font-size:10px\'>\n' + timestamp + '\n</p>\n' + '<p>\n' + e + '\n</p>\n</div>\n<br>\n\n'
+b[38] += f
+h = open("index.htm", "w")
+h.writelines(b)
+h.close()
+i = open("feed.xml", "r")
+l = i.readlines()
+m = '\n<item>\n' + '<description>\n' + e + '\n</description>\n' + '<link>\n' + 'https://mrzgardini.github.io/writings/index.htm#' + timestamp + '\n</link>\n</item>\n\n'
+l[3] += m
+n = open("feed.xml", "w")
+n.writelines(l)
+n.close()
