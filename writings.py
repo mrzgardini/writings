0b27f0ba6768c1144030011ec0912a1ff6e588d2
mrzgardini <33785639+mrzgardini@users.noreply.github.com>
2024-04-05 18:28:32 +0200

    Update writings.py

--- writings.py
+++ writings.py
@@ -8,7 +8,7 @@ b = a.readlines()
 c = open('writings/input.txt', 'r')
 d = c.read()
 e = re.sub(f'\n', f'<br> ', d)
-f = '\n<div class=\'data\'' + ' ' + 'id=\'' + timestamp + '\'>\n' + timestamp + '\n' + '<div>\n' + e + '\n</div>\n</div>\n<br>\n\n'
+f = '\n<div class=\'data\'' + ' ' + 'id=\'' + timestamp + '\'>\n' + timestamp + '\n' + '</div>\n<div>\n' + e + '\n</div>\n<br>\n\n'
 b[55] += f
 h = open('mrzgardini.github.io/writings/index.htm', 'w')
 h.writelines(b)
